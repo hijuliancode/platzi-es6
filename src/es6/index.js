b4beb1adfdc0792f1a3e8d72666bcbc6e842e895
JulianCode <hijuliansosa@protonmail.com>
2020-05-15 00:19:53 -0500

    ES6: Classes

--- src/es6/index.js
+++ src/es6/index.js
@@ -97,3 +97,19 @@ helloPromise()
   .then(response => console.log(response))
   .then(() => console.log('hola!'))
   .catch(error => console.log(error));
+
+// Classes
+class calculator {
+  constructor() {
+    this.valueA = 0
+    this.valueB = 0
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator()
+console.log(calc.sum(20, 10))
