7efd30f17fbac82ca40a819525806652eb9afb92
JulianCode <hijuliansosa@protonmail.com>
2020-05-15 01:33:10 -0500

    ES8: Async & Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -24,3 +24,24 @@ console.log(values.length)
 const string = 'hello';
 console.log(string.padStart(7, 'hi'))
 console.log(string.padEnd(12, ' -----'))
+
+// Async & Await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (false)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'))
+  })
+}
+
+const helloAsync = async () => {
+  try {
+    const hello = await helloWorld()
+    console.log(hello)
+  }
+  catch (error) {
+    console.log('ERRORR => ', error)
+  }
+}
+
+helloAsync()
