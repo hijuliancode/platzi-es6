8642e6c85ae9d0cad547568c165ebead9e47d6c3
JulianCode <hijuliansosa@protonmail.com>
2020-05-15 01:09:31 -0500

    ES8: Object.entries

---
+++ src/es8/index.js
@@ -0,0 +1,10 @@
+// Object.entries
+const data = {
+  frontend: 'Ginna',
+  backend: 'Julian',
+  designer: 'Meraki'
+}
+
+const entries = Object.entries(data)
+console.log(entries)
+console.log(entries.length)
